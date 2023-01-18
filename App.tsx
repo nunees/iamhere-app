824e71b24ce258e18370ea69b33341d4f4081fbc
nunees <felipe_silva_nunes@hotmail.com>
2023-01-18 17:08:10 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function App(){
+  return(
+    <View>
+      <Text>React Native</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  
+});
