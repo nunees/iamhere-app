83446b5b0b4a9be1421840982c42266df1886b0c
nunees <felipe_silva_nunes@hotmail.com>
2023-01-18 17:48:19 -0300

    feat: add initial components

--- App.tsx
+++ App.tsx
@@ -2,12 +2,30 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function App(){
   return(
-    <View>
-      <Text>React Native</Text>
+    <View style={{
+      backgroundColor: "#131016",
+      flex: 1,
+      padding: 24
+    }}>
+      <Text style={{
+        color: "#fdfcfe",
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48,
+        }}>
+          Nome do evento
+      </Text>
+
+      <Text style={{
+        color: '#6B6B6B',
+        fontSize: 16
+      }}>
+        Quarta, 18 de Janeiro de 2023.
+      </Text>
     </View>
   );
 }
 
 const style = StyleSheet.create({
-  
+
 });
