d52347b7fa207ba6e3092c8f5150eb5df949fbda
nunees <felipe_silva_nunes@hotmail.com>
2023-01-25 18:49:52 -0300

    fix: flexbox direction

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,20 +1,33 @@
 import React from "react";
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from "./style";
 
 export default function Home() {
+  function handleParticipantAdd() {
+    console.log("Clicou no botao de adicionar");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
 
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder={"Nome do participante"}
-        placeholderTextColor={"#6B6B6B"}
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder={"Nome do participante"}
+          placeholderTextColor={"#6B6B6B"}
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={(e) => console.log("clicou em adicionar")}
+        >
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
